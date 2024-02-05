c9c348fe96e46a5f43853369640ba98ca4ad46a4
Pablo Lopez Sardi <lopezsardip@gmail.com>
2024-02-05 00:35:08 -0300

    components creados y encabezado implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Encabezado from './components/Encabezado'
+import Lista from './components/Lista'
+import TextField from './components/TextField'
 
 function App() {
 
   return (
     <>
-      
+      <section className='mx-auto mt-5 w-50 text-center bg-light rounded-3 h-auto p-3'>
+        <Encabezado></Encabezado>
+        <TextField></TextField>
+        <Lista></Lista>
+      </section>
     </>
   )
 }
---
+++ src/components/Encabezado.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Encabezado = () => {
+    return (
+        <>
+            <h1>Lista de Tareas</h1> 
+            <p>Ingresa tus tareas</p>  
+        </>
+    );
+};
+
+export default Encabezado;
